b0531c3b4e6e050d25356cd38a74fce8e97d9e32
bodeus <chirairobb@gmail.com>
2026-03-14 13:09:17 +0200

    f1

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ console. log("Hello World");
 console.log("here comes trouble");
 >>>>>>> trouble
 alert("uh-oh!")
-alert("i fixed the problem")
 alert("i fixed the problem1")
+alert("i fixed the problem")
